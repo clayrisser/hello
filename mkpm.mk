4f64f205bdf2490e8a9fa1ef039a2323b44057e6
Clay Risser <clayrisser@gmail.com>
2021-09-27 19:42:19 -0500

    Error when offline

--- mkpm.mk
+++ mkpm.mk
@@ -3,7 +3,7 @@
 # File Created: 27-09-2021 17:45:48
 # Author: Clay Risser
 # -----
-# Last Modified: 27-09-2021 19:19:16
+# Last Modified: 27-09-2021 19:42:12
 # Modified By: Clay Risser
 # -----
 # BitSpur Inc (c) Copyright 2021
@@ -50,7 +50,7 @@ $(MKPM_PACKAGE_DIR)/.bootstrap.mk:
 	@$(MKDIR_P) $(MKPM_PACKAGE_DIR)
 	@cd $(MKPM_PACKAGE_DIR) && \
 		$(shell curl --version >$(NULL) 2>$(NULL) && \
-			echo curl -Ls -o || \
-			echo wget -q --content-on-error -O) \
+			echo curl -L -o || \
+			echo wget --content-on-error -O) \
 		.bootstrap.mk $(MKPM_BOOTSTRAP) >$(NULL)
 ############## MKPM BOOTSTRAP SCRIPT END ##############
